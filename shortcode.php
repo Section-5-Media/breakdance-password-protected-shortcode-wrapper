13e60e2e632973c83a872a42e4171633654506d2
nick-s5 <99763054+nick-s5@users.noreply.github.com>
2024-08-20 12:19:11 -0500

    Allow for GET URL parameter to bypass the form

--- shortcode.php
+++ shortcode.php
@@ -1,36 +1,43 @@
-<?php 
+<?php
 function custom_password_shortcode($atts, $content = null) {
     // Extract attributes
     extract(shortcode_atts(array(
         'password_field' => 'custom_password',
     ), $atts));
 
-    // Check if the password has been submitted
-    // I am using Metabox.io for custom fields - if you are not then replace "rwmb_meta('')" with your custom field accessor
-    if(isset($_POST['custom_password_submit'])) {
-        if(isset(rwmb_meta('password-protected')) && sanitize_text_field($_POST['custom_password']) === rwmb_meta('password-protected')) {
-            // Correct password entered, set a session variable
-            session_start();
-            $_SESSION['custom_password_' . get_the_ID()] = true;
-        }
+    // Start the session
+    session_start();
+
+    // Fetch the password from the custom field using MetaBox.io or another method
+    $stored_password = rwmb_meta('password-protected');
+
+    // Check if the password has been submitted via POST or exists in the URL as a parameter
+    $input_password = '';
+    if (isset($_POST['custom_password_submit'])) {
+        $input_password = sanitize_text_field($_POST['custom_password']);
+    } elseif (isset($_GET['password'])) {
+        $input_password = sanitize_text_field($_GET['password']);
     }
 
-    session_start();
-    if((isset($_SESSION['custom_password_' . get_the_ID()]) && $_SESSION['custom_password_' . get_the_ID()] === true) || rwmb_meta('password-protected') == '') {
-        // If password custom field empty or correct password is entered, display the content
+    // If the correct password is entered or provided via the URL, set the session variable
+    if ($input_password && $input_password === $stored_password) {
+        $_SESSION['custom_password_' . get_the_ID()] = true;
+    }
+
+    // Check if the session variable is set, or if the password field is empty (no protection)
+    if ((isset($_SESSION['custom_password_' . get_the_ID()]) && $_SESSION['custom_password_' . get_the_ID()] === true) || $stored_password == '') {
+        // If the password field is empty or the correct password is entered, display the content
         return do_shortcode($content);
     } else {
-        // Display password form
+        // Display the password form if the password is incorrect or not yet entered
         return '
             <div class="password-protected-wrapper"><h2>Password Required</h2>
             <form action="' . get_permalink() . '" method="post">
-                    <input type="password" name="custom_password" />
-                    <input type="submit" name="custom_password_submit" value="Enter" />
-                </form></div>';
+                <input type="password" name="custom_password" />
+                <input type="submit" name="custom_password_submit" value="Enter" />
+            </form></div>';
     }
 }
 
 add_shortcode('custom_protected', 'custom_password_shortcode');
-
-
 ?>
